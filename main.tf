8bc2b725b867067ada4cc28217f82b2b8f2803f3
emmanuel nocquet <121935535+enocquet@users.noreply.github.com>
2023-12-28 17:49:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ data "terraform_remote_state" "state" {
 
 module "cosmotech-tenant" {
   source  = "Cosmo-Tech/cosmotech-tenant/azure"
-  version = "0.2.7"
+  version = "0.2.8"
 
   subscription_id         = var.subscription_id
   tenant_id               = var.tenant_id
