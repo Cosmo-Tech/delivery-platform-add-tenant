3abc6e01c41e6ccd4c57370f5ca1c4ef2cf7e565
enocquet <emmanuelnocquet8@gmail.com>
2024-03-20 17:51:24 +0100

    Compatibility with new major module release

--- Readme.md
+++ Readme.md
@@ -70,19 +70,28 @@ Entries will automatically be added at the end of the platform creation into the
 
 The module needs at least these values:
 ```tf
-subscription_id = ""
-tenant_id       = ""
-owner_list      = [""]
-project_name    = ""
-cluster_name    = ""
-platform_vnet   = ""
-vnet_iprange    = "10.10.0.0/16"
-platform_sp_name        = ""
-platform_resource_group = ""
-platform_public_ip      = ""
-tenant_resource_group   = ""
-dns_record              = ""
-namespace               = ""
+subscription_id = "<AzureSubscriptionId>"
+tenant_id       = "<AzureTenantId>"
+
+# Already existing resources
+api_dns_name            = "<example.cosmotech.com>"
+network_client_id       = "<PlatformNetworkAppRegistrationClientId>"
+publicip_resource_group = "<ResourceGroupName>"
+vnet_resource_group     = "<ResourceGroupName>"
+cluster_name            = "<AKSClusterName>"
+vnet_name               = "<PlatformVnetName>"
+tenant_sp_name          = "<PlatformServicePrincipalName>"
+common_resource_group   = "<PlatformResourceGroupName>"
+public_ip_name          = "<PlatformPublicIPName>"
+dns_record              = "<DnsRecordName>"
+
+# New entries for this tenant
+tenant_virtual_network_address_prefix = "10.10.0.0/16"
+tenant_resource_group                 = "<TenantResourceGroupName>"
+kubernetes_tenant_namespace           = "<TenantKubernetesNamespaceName>"
+cosmotech_api_version                 = "3.1.0"
+project_name                          = ""
+owner_list                            = [""]
 ```
 
 Fill the terraform.tfvars file with your values. For explanations, see [below](#5.).
@@ -130,13 +139,15 @@ Here, two parts:
 
 These values already exist and come from the common resources of the Cosmo Tech Platform. You can retrieve them through the Azure portal or from the outputs of this [module](https://registry.terraform.io/modules/Cosmo-Tech/cosmotech-common/azure/latest)
 ```tf
-subscription_id = ""
-tenant_id       = ""
-cluster_name    = ""              # The name of the AKS cluster hosting our platform
-platform_vnet   = ""              # The cluster's virtual network
-platform_sp_name        = ""      # The Platform Service Principal's name
-platform_resource_group = ""      # The Platform's resource group
-platform_public_ip      = ""      # The Platform public IP
+api_dns_name            = ""      # The domain name for the platform (should be like <dns_record.subdomain.domain.com>)
+network_client_id       = ""      # The Network App Registration clientID
+publicip_resource_group = ""      # The resource group name hosting the existing PublicIP
+vnet_resource_group     = ""      # The resource group name hosting the existing VNET
+cluster_name            = ""      # The name of the AKS cluster hosting our platform
+vnet_name               = ""      # The cluster's virtual network
+tenant_sp_name          = ""      # The Platform Service Principal's name
+common_resource_group   = ""      # The Platform's resource group
+public_ip_name          = ""      # The Platform public IP name
 dns_record              = ""      # The DNS record of the Platform
 ```
 
@@ -144,10 +155,11 @@ dns_record              = ""      # The DNS record of the Platform
 ## 5.2. Values specifics to this tenant
 
 ```tf
-owner_list      = [""]
-project_name    = ""              # The name of your project
-vnet_iprange    = "10.10.0.0/16"  # The IP range of the virtual network we want to build for this tenant. This should be unique through the AKS cluster
-tenant_resource_group   = ""      # The Azure resource group that will hold all created resources for this tenant
-namespace               = ""      # The Kubernetes namespace for the tenant. Should be the same as the *tenant_resource_group*
+tenant_virtual_network_address_prefix = ""      # The IP range of the virtual network we want to build for this tenant. This should be unique through the AKS cluster
+tenant_resource_group                 = ""      # The Azure resource group that will hold all created resources for this tenant
+kubernetes_tenant_namespace           = ""      # The Kubernetes namespace for the tenant. Should be the same as the *tenant_resource_group*
+cosmotech_api_version                 = ""      # The Cosmo Tech API Version you wish to install
+owner_list                            = [""]
+project_name                          = ""      # The name of your project (/!\ Any changes on this parameter may change a LOT of things in your infrastructure)
 ```
 
--- main.tf
+++ main.tf
@@ -5,39 +5,101 @@ data "terraform_remote_state" "state" {
     resource_group_name  = var.tf_resource_group_name
     storage_account_name = var.tf_storage_account_name
     container_name       = var.tf_container_name
-    key                  = var.tf_blob_name
+    key                  = var.tf_blob_name_tenant
     access_key           = var.tf_access_key
   }
 }
 
 module "cosmotech-tenant" {
   source  = "Cosmo-Tech/cosmotech-tenant/azure"
-  version = "0.2.10"
+  version = "1.1.1"
 
-  subscription_id         = var.subscription_id
-  tenant_id               = var.tenant_id
-  client_id               = var.client_id
-  client_secret           = var.client_secret
-  tls_secret_name         = var.tls_secret_name
-  namespace               = var.namespace
-  monitoring_namespace    = var.monitoring_namespace
-  chart_package_version   = var.chart_package_version
-  owner_list              = var.owner_list
-  project_name            = var.project_name
-  cluster_name            = var.cluster_name
-  platform_vnet           = var.platform_vnet
-  vnet_iprange            = var.vnet_iprange
-  platform_sp_name        = var.platform_sp_name
-  platform_resource_group = var.platform_resource_group
-  platform_public_ip      = var.platform_public_ip
-  tenant_resource_group   = var.tenant_resource_group
-  dns_record              = var.dns_record
-  create_vault_entries    = var.create_vault_entries
-  vault_addr              = var.vault_addr
-  vault_token             = var.vault_token
-  deployment_type         = var.deployment_type
-  platform_group_id       = var.platform_group_id
-  networkadt_sp_object_id = var.networkadt_sp_object_id
-  principal_id            = var.platform_tenant_object_id
-  location                = var.location
+  # Azure tenant prerequisites
+  tenant_id                         = var.tenant_id
+  client_id                         = var.client_id
+  client_secret                     = var.client_secret
+  platform_url                      = var.platform_url
+  identifier_uri                    = var.identifier_uri
+  project_stage                     = var.project_stage
+  project_name                      = var.project_name
+  owner_list                        = var.owner_list
+  audience                          = var.audience
+  webapp_url                        = var.webapp_url
+  location                          = var.location
+  tenant_resource_group             = var.tenant_resource_group
+  common_resource_group             = var.common_resource_group
+  dns_record                        = var.dns_record
+  dns_zone_name                     = var.dns_zone_name
+  dns_zone_rg                       = var.dns_zone_rg
+  api_version_path                  = var.api_version_path
+  customer_name                     = var.customer_name
+  user_app_role                     = var.user_app_role
+  image_path                        = var.image_path
+  create_restish                    = var.create_restish
+  create_powerbi                    = var.create_powerbi
+  create_secrets                    = var.create_secrets
+  create_webapp                     = var.create_webapp
+  create_babylon                    = var.create_babylon
+  cost_center                       = var.cost_center
+  publicip_resource_group           = var.publicip_resource_group
+  vnet_name                         = var.vnet_name
+  public_ip_name                    = var.public_ip_name
+  kubernetes_azurefile_storage_tags = var.kubernetes_azurefile_storage_tags
+
+  # Azure tenant resources
+  deployment_type                              = var.deployment_type
+  subscription_id                              = var.subscription_id
+  tenant_virtual_network_address_prefix        = var.tenant_virtual_network_address_prefix
+  tenant_virtual_subnet_network_address_prefix = var.tenant_virtual_subnet_network_address_prefix
+  managed_disk_name                            = var.managed_disk_name
+  cluster_name                                 = var.cluster_name
+  network_sp_object_id                         = var.network_sp_object_id
+  storage_kind                                 = var.storage_kind
+  vnet_resource_group                          = var.vnet_resource_group
+  create_backup                                = var.create_backup
+  create_cosmosdb                              = var.create_cosmosdb
+  create_adx                                   = var.create_adx
+
+  blob_privatedns_zonename     = var.blob_privatedns_zonename
+  queue_privatedns_zonename    = var.queue_privatedns_zonename
+  table_privatedns_zonename    = var.table_privatedns_zonename
+  eventhub_privatedns_zonename = var.eventhub_privatedns_zonename
+  adt_privatedns_zonename      = var.adt_privatedns_zonename
+
+  redis_disk_tier     = var.redis_disk_tier
+  redis_disk_sku      = var.redis_disk_sku
+  redis_disk_size_gb  = var.redis_disk_size_gb
+  kusto_instance_type = var.kusto_instance_type
+  kustonr_instances   = var.kustonr_instances
+  auto_stop_kusto     = var.auto_stop_kusto
+  tenant_group_id     = var.tenant_group_id
+  tenant_sp_object_id = var.tenant_sp_object_id
+
+  # Vault entries
+  kubernetes_tenant_namespace = var.kubernetes_tenant_namespace
+  organization_name           = var.organization_name
+  vault_addr                  = var.vault_addr
+  vault_token                 = var.vault_token
+
+  # Platform tenant resources
+  api_dns_name                       = var.api_dns_name
+  api_replicas                       = var.api_replicas
+  tls_certificate_type               = var.tls_certificate_type
+  tls_certificate_custom_certificate = var.tls_certificate_custom_certificate
+  tls_certificate_custom_key         = var.tls_certificate_custom_key
+  monitoring_enabled                 = var.monitoring_enabled
+  monitoring_namespace               = var.monitoring_namespace
+  chart_package_version              = var.chart_package_version
+  redis_disk_name                    = var.redis_disk_name
+  redis_port                         = var.redis_port
+  argo_minio_persistence_size        = var.argo_minio_persistence_size
+  argo_minio_requests_memory         = var.argo_minio_requests_memory
+  archive_ttl                        = var.archive_ttl
+  cluster_issuer_name                = var.cluster_issuer_name
+  cosmotech_api_version              = var.cosmotech_api_version
+  cosmotech_api_ingress_enabled      = var.cosmotech_api_ingress_enabled
+  network_client_id                  = var.network_client_id
+  network_client_secret              = var.network_client_secret
+  tenant_client_id                   = var.tenant_client_id
+  tenant_client_secret               = var.tenant_client_secret
 }
