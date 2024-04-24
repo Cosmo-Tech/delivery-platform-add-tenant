6a46d505fee325cd69a348af93d0066bcdf22de6
Sylvain Joubert <sylvain.joubert@cosmotech.com>
2024-04-24 11:07:16 +0200

    Update generic version to 1.2.0

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ data "terraform_remote_state" "state" {
 
 module "cosmotech-tenant" {
   source  = "Cosmo-Tech/cosmotech-tenant/azure"
-  version = "1.1.13"
+  version = "1.2.0"
 
   # Azure tenant prerequisites
   tenant_id                              = var.tenant_id
