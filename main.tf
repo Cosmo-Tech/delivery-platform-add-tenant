2724916dbd2d877ef46ef39a1c495742604c4abd
Sylvain Joubert <sylvain.joubert@cosmotech.com>
2024-04-24 13:08:58 +0200

    Update dependency version to 1.2.1

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ data "terraform_remote_state" "state" {
 
 module "cosmotech-tenant" {
   source  = "Cosmo-Tech/cosmotech-tenant/azure"
-  version = "1.2.0"
+  version = "1.2.1"
 
   # Azure tenant prerequisites
   tenant_id                              = var.tenant_id
